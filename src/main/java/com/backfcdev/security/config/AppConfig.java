acfd3fbcd725a7b94ce6231ade077a6660982bbd
gibbssonfarias30 <gibbfarc17@gmail.com>
2023-06-01 22:15:20 -0500

    the UserDetailsService bean was made to implement its method for searching the username from the database

---
+++ src/main/java/com/backfcdev/security/config/AppConfig.java
@@ -0,0 +1,24 @@
+package com.backfcdev.security.config;
+
+
+import com.backfcdev.security.repository.IUserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+
+@RequiredArgsConstructor
+@Configuration
+public class AppConfig {
+
+    private final IUserRepository userRepository;
+
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return username -> userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Username not found"));
+    }
+}
