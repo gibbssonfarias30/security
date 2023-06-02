0f13761b47fe33c1333b1817ed781d19d4c4df45
gibbssonfarias30 <gibbfarc17@gmail.com>
2023-06-02 08:08:40 -0500

    a demo controller has been created as an example to access an endpoint in case the user is authenticated

---
+++ src/main/java/com/backfcdev/security/controller/DemoController.java
@@ -0,0 +1,18 @@
+package com.backfcdev.security.controller;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/demo")
+public class DemoController {
+
+    @GetMapping
+    ResponseEntity<String> sayHello(){
+        return ResponseEntity.ok("Hello, you have accessed the secure endpoint!");
+    }
+
+}
